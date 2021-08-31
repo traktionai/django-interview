5b210fd614e48261f77e97e07a2a42644c88a9b7
Luis Muñoz Villarreal <luis@traktion.ai>
2021-08-31 22:43:45 +0100

    Django Interview Skeleton

---
+++ apps/users/script.py
@@ -0,0 +1,17 @@
+USERS = [
+    # (ID, Username),
+    (1, "Bret"),
+    (2, "Antonette"),
+    (3, "Samantha"),
+    (4, "Karianne"),
+    (5, "Kamren"),
+    (6, "Leopoldo_Corkery"),
+    (7, "Elwyn.Skiles"),
+    (8, "Maxime_Nienow"),
+    (9, "Delphine"),
+    (10, "Moriah.Stanton"),
+]
+
+
+def run():
+    # write your code here
